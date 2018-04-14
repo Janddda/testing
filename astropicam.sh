5f5815930c84011d6d9430f5023170e5acf3c2cd
Janddda <janddda@yahoo.com>
2018-04-14 22:18:14 +0200

    Update astropicam.sh

--- astropicam.sh
+++ astropicam.sh
@@ -1,5 +1,5 @@
 !#/bin/sh
-
+#from http://shortcircuitsandinfiniteloops.blogspot.cz/2017/10/raspberry-pi-skycam-w-noir-v2-camera.html
 cd /data/media/sda/
 mkdir "$(date +"%d-%m-%Y")"
 raspistill -o "$(date +"%d-%m-%Y")"/sky_%04d.jpg -t 500000000 -tl 0 -ss 9900000 -ISO 800 -$
